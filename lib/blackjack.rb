d8dd8e25133326b9346d2b3ce6bb16d90abe173c
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-16 08:37:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,9 +30,9 @@ def hit?
   hand = 11
   choice = ""
   prompt_user
-  until choice !== "h" || "s"
+  until choice != "h" || "s"
     choice = get_user_input 
-    if choice !== "h" || "s"
+    if choice != "h" || "s"
       invalid_command
     end
   end
